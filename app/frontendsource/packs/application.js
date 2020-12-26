6246dd31637bf7e23143f8227eaa2176bcad6e14
superchen <chensuper0202@gmail.com>
2020-12-26 14:46:45 +0800

    fix the webpacker upload image problem

--- app/frontendsource/packs/application.js
+++ app/frontendsource/packs/application.js
@@ -11,6 +11,10 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+
 import "controllers";
 
 import "scripts/shared";
