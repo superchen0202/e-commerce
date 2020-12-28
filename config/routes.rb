94dedaef7643a6beec27f30fcdaec6f3c35d4fd0
superchen <chensuper0202@gmail.com>
2020-12-28 12:24:00 +0800

    subscribe, product-list, product-show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
-  
-  
-  
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:show, :index]
+
+
 end
