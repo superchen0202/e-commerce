0495c763a012f5a136d935df16a832dbdd947344
superchen <chensuper0202@gmail.com>
2021-01-02 21:26:47 +0800

    C R U

--- app/controllers/admin/vendors_controller.rb
+++ app/controllers/admin/vendors_controller.rb
@@ -1,4 +1,6 @@
 class Admin::VendorsController < Admin::BaseController
+    
+    before_action :find_vendor, only:[:edit, :update, :destroy]
 
     def index
         @vendors = Vendor.order('created_at DESC')
@@ -22,17 +24,29 @@ class Admin::VendorsController < Admin::BaseController
     end 
 
     def edit
+        
     end
 
     def update
+
+        if @vendor.update(vendor_params)
+            redirect_to edit_admin_vendor_path, notice: "更新成功"
+        else
+            render :edit
+        end
+
     end
 
     def destroy
     end
 
-
+    private
     def vendor_params
         params.require(:vendor).permit(:title, :description, :online)
+    end
+
+    def find_vendor
+        @vendor = Vendor.find(params[:id])
     end
 
 end
