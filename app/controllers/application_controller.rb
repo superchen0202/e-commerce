960d6b0d202c733dfec483850511f9f383f594f7
superchen <chensuper0202@gmail.com>
2021-01-02 22:15:24 +0800

    rescue the 404 not found information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    def record_not_found
+        
+        render file: "#{Rails.root}/public/404.html",
+        layout: false,
+        status: false
+
+    end
+
 end
