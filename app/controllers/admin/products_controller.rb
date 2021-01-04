9490322e027cefc5784c4b5c63f870c1b0408ab9
superchen <chensuper0202@gmail.com>
2021-01-04 16:50:27 +0800

    add product form at backsatge, but create not yet

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,23 @@
 class Admin::ProductsController < Admin::BaseController
 
     def index
+        @products = Product.all
     end
 
-    
+    def new
+        @product = Product.new
+    end
+
+    def create
+    end 
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,5 +4,6 @@ class Vendor < ApplicationRecord
 
     validates :title, presence: true
 
+    scope :available, -> {where(online: true)}
 
 end
