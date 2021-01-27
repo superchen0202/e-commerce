94cb7cdfdc2ee170e9c37792414ab261b20083e1
superchen <chensuper0202@gmail.com>
2021-01-27 12:04:01 +0800

    TDD all case code arranged

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
 
+
+    let(:cart){Cart.new}
+
     describe "基本功能" do 
     
         #case 1
         it "可以把商品丟購物車，購物車就會有東西" do
-            cart = Cart.new
+            # cart = Cart.new
             cart.add_items(1)
             expect(cart.emtpy?).to be false    
         end
@@ -14,7 +17,7 @@ RSpec.describe Cart, type: :model do
         #case 2
         it "加了相同種類的商品到購物車裡，購買項目並不會增加，但商品的數量會改變" do
             
-            cart = Cart.new
+            # cart = Cart.new
             
             cart.add_items(7, 3)                      # 3.times{cart.add_items(7)}
             cart.add_items(4, 5)                      # 5.times{cart.add_items(4)}
@@ -28,7 +31,7 @@ RSpec.describe Cart, type: :model do
         #case 3
         it "商品可以放到購物車裡，也可以再拿出來" do 
             
-            cart = Cart.new
+            # cart = Cart.new
             p1 = FactoryBot.create(:product)
             
             cart.add_items(p1.id)
@@ -39,7 +42,7 @@ RSpec.describe Cart, type: :model do
         #case 4
         it "每個 Cart Item 都可以計算它自己的金額(小計)" do
             
-            cart = Cart.new
+            # cart = Cart.new
 
             p1 = FactoryBot.create(:product, sell_price: 5)
             p2 = FactoryBot.create(:product, sell_price: 9)
@@ -55,7 +58,7 @@ RSpec.describe Cart, type: :model do
         #case 5
         it "可以計算整台購物車的總消費金額" do
 
-            cart = Cart.new
+            # cart = Cart.new
 
             p1 = FactoryBot.create(:product, sell_price: 5)
             p2 = FactoryBot.create(:product, sell_price: 9)
@@ -76,14 +79,14 @@ RSpec.describe Cart, type: :model do
             # p cart.special
         end
 
-      end 
+    end 
 
-
-      describe "進階功能" do
+    describe "進階功能" do
 
         #case 1
         it "可以將購物車內容轉換成 Hash 並存到 Session 裡" do
-            cart = Cart.new
+            
+            # cart = Cart.new
 
             p1 = FactoryBot.create(:product)
             p2 = FactoryBot.create(:product)
@@ -105,18 +108,17 @@ RSpec.describe Cart, type: :model do
 
         end
 
-      end
-
-
-      def cart_hash
+        def cart_hash
         
-        return cart_content = {
-            items: [
-                {"product_id": 2, "quantity": 3}, 
-                {"product_id": 4, "quantity": 7}
-            ]
-        }
+            return cart_content = {
+                items: [
+                    {"product_id": 2, "quantity": 3}, 
+                    {"product_id": 4, "quantity": 7}
+                ]
+            }
+    
+        end
 
-      end
+    end
 
 end
