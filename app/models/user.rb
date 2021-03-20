1f8695b7b6af250f982345869fddaa480f47cca8
superchen <chensuper0202@gmail.com>
2021-03-20 18:59:59 +0800

    Heroku ver.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   }
 
 
+  # google_oauth2
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
